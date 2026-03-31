966f1f47a0c08a1858ff2c5fa1ebd68a6e107a17
kalycaokta-ux <kalycaokta@gmail.com>
2026-03-31 20:24:13 +0700

    menambahkan pendaftaran jurusan kampus

--- switchCase.cpp
+++ switchCase.cpp
@@ -2,3 +2,11 @@
 using namespace std;
 
 // Deklarasi variable global
+int kodeJurusan;
+string namaJurusan;
+string statusKuota;
+
+// prosedur input
+void input() {
+    cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
+}
