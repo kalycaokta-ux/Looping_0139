abf8e35ee50647959b6fbcda867563ae3aa472a9
kalycaokta-ux <kalycaokta@gmail.com>
2026-03-31 20:28:46 +0700

    menambahkan Sistem Informasi

--- switchCase.cpp
+++ switchCase.cpp
@@ -11,4 +11,5 @@ void input() {
     cout << "=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
     cout << "1. Teknologi Informasi" << endl;
     cout << "2. Teknik Elektro" << endl;
+    cout << "3. Sistem Informasi" << endl;
 }
